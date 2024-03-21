2de7c6be81e9ae097047282d55e4f2753d149209
Kr1ss22 <144793177+Kr1ss22@users.noreply.github.com>
2024-03-21 11:38:56 +0200

    Add files via upload

---
+++ Yl2.gd
@@ -0,0 +1,13 @@
+#Kristjan Mustkivi 21.03
+var money = 50
+var price = 100
+var a = 10
+var b = 20
+
+func ready():
+	if money < price:
+		print(price-money, "raha jääb puudu")
+		else:
+			print ("toode on sinu!")
+			
+		
